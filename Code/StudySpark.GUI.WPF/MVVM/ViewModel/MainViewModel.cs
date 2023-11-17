d29d4623d1ca2e2f351faeafacd25913eab1e798
Sven Arends <github@svenar.nl>
2023-11-17 11:48:58 +0100

    Progress on implementing WPF MVVM base

---
+++ Code/StudySpark.GUI.WPF/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,96 @@
+﻿using StudySpark.GUI.WPF.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace StudySpark.GUI.WPF.MVVM.ViewModel {
+    internal class MainViewModel : ObservableObject {
+
+        public RelayCommand OverviewViewCommand { get; set; }
+        public RelayCommand NotesViewCommand { get; set; }
+        public RelayCommand FilesViewCommand { get; set; }
+        public RelayCommand TimelineViewCommand { get; set; }
+        public RelayCommand ScheduleViewCommand { get; set; }
+
+        public RelayCommand MinimizeCommand { get; private set; }
+        public RelayCommand MaximizeCommand { get; private set; }
+        public RelayCommand CloseCommand { get; private set; }
+
+        public OverviewViewModel OverviewVM { get; set; }
+        public NotesViewModel NotesVM { get; set; }
+        public FilesViewModel FilesVM { get; set; }
+        public ScheduleViewModel ScheduleVM { get; set; }
+        public TimelineViewModel TimelineVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView {
+            get { return _currentView; }
+            set {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel() {
+            MinimizeCommand = new RelayCommand(o => MinimizeWindow());
+            MaximizeCommand = new RelayCommand(o => MaximizeWindow());
+            CloseCommand = new RelayCommand(o => CloseWindow());
+
+            OverviewVM = new OverviewViewModel();
+            NotesVM = new NotesViewModel();
+            FilesVM = new FilesViewModel();
+            ScheduleVM = new ScheduleViewModel();
+            TimelineVM = new TimelineViewModel();
+
+            CurrentView = FilesVM; // OverviewVM; // TODO: Change when we actually have an overview view
+
+            OverviewViewCommand = new RelayCommand(o => {
+                CurrentView = OverviewVM;
+            });
+
+            NotesViewCommand = new RelayCommand(o => {
+                CurrentView = NotesVM;
+            });
+
+            FilesViewCommand = new RelayCommand(o => {
+                CurrentView = FilesVM;
+            });
+
+            TimelineViewCommand = new RelayCommand(o => {
+                CurrentView = TimelineVM;
+            });
+
+            ScheduleViewCommand = new RelayCommand(o => {
+                CurrentView = ScheduleVM;
+            });
+
+        }
+
+        private void MinimizeWindow() {
+            if (Application.Current.MainWindow != null) {
+                Application.Current.MainWindow.WindowState = WindowState.Minimized;
+            }
+        }
+
+        private void MaximizeWindow() {
+            if (Application.Current.MainWindow != null) {
+                if (Application.Current.MainWindow.WindowState == WindowState.Maximized) {
+                    Application.Current.MainWindow.WindowState = WindowState.Normal;
+                } else {
+                    Application.Current.MainWindow.WindowState = WindowState.Maximized;
+                }
+            }
+        }
+
+        private void CloseWindow() {
+            if (Application.Current.MainWindow != null) {
+                Application.Current.MainWindow.Close();
+            }
+        }
+    }
+}
--- Code/StudySpark.GUI.WPF/MainWindow.xaml.cs
+++ Code/StudySpark.GUI.WPF/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,9 +19,32 @@ namespace StudySpark.GUI.WPF {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
+    public partial class MainWindow : Window, INotifyPropertyChanged {
+
+        private string searchbarPath = "None";
+
+        public string SearchbarPath {
+            get { return searchbarPath; }
+            set {
+                searchbarPath = value;
+                OnPropertyChanged(nameof(SearchbarPath));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void WindowDrag(object sender, MouseButtonEventArgs e) {
+            if (e.LeftButton == MouseButtonState.Pressed) {
+                DragMove();
+            }
         }
     }
 }
