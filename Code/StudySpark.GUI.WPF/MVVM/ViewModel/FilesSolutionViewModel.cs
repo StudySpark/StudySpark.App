08354fbbf3f6d88ed5308183e730ef8c2b0a91ec
Sven Arends <github@svenar.nl>
2023-11-20 19:58:34 +0100

    packing uri for image url

--- Code/StudySpark.GUI.WPF/MVVM/ViewModel/FilesSolutionViewModel.cs
+++ Code/StudySpark.GUI.WPF/MVVM/ViewModel/FilesSolutionViewModel.cs
@@ -7,18 +7,14 @@ using StudySpark.Core.FileManager;
 using StudySpark.GUI.WPF.Core;
 using System.Windows;
 
-namespace StudySpark.GUI.WPF.MVVM.ViewModel
-{
-    internal class FilesSolutionViewModel : ObservableObject
-    {
+namespace StudySpark.GUI.WPF.MVVM.ViewModel {
+    internal class FilesSolutionViewModel : ObservableObject {
         private object _currentSLNList;
         public object CurrentSLNList {
-            get
-            {
+            get {
                 return _currentSLNList;
             }
-            set
-            {
+            set {
                 _currentSLNList = value;
             }
         }
@@ -27,12 +23,9 @@ namespace StudySpark.GUI.WPF.MVVM.ViewModel
         private List<string> _recentSLNFiles = SearchFiles.GetFilesFromRecent(".sln.lnk", System.IO.SearchOption.TopDirectoryOnly);
         StackPanel solutionPanel = new();
 
-        public FilesSolutionViewModel()
-        {
-            for (int i = 0; i < AmountToShow; i++)
-            {
-                Button b = new Button
-                {
+        public FilesSolutionViewModel() {
+            for (int i = 0; i < AmountToShow; i++) {
+                Button b = new Button {
                     Width = 50,
                     Height = 50,
                 };
@@ -40,19 +33,16 @@ namespace StudySpark.GUI.WPF.MVVM.ViewModel
                 b.Background = SetIcon();
 
                 solutionPanel.Children.Add(b);
-                solutionPanel.Children.Add(new TextBox
-                {
+                solutionPanel.Children.Add(new TextBox {
                     Text = _recentSLNFiles[i]
-                }); 
+                });
             }
             _currentSLNList = solutionPanel;
         }
 
-        public ImageBrush SetIcon()
-        {
-            var brush = new ImageBrush
-            {
-                ImageSource = new BitmapImage(new Uri("..\\..\\..\\Images\\Icon_VS.png", UriKind.Relative))
+        public ImageBrush SetIcon() {
+            var brush = new ImageBrush {
+                ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/Icon_VS.png"))
             };
             return brush;
         }
