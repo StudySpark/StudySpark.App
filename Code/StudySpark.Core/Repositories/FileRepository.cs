59c8843c3f62a37dc55b4360f7c07db6831cff85
CarlijnGJ <carlijn.blom@windesheim.nl>
2023-11-22 11:26:19 +0100

    Filetree light cleanup

--- Code/StudySpark.Core/Repositories/FileRepository.cs
+++ Code/StudySpark.Core/Repositories/FileRepository.cs
@@ -9,27 +9,27 @@ using System.Data;
 using System.Diagnostics;
 using System.Reflection.Metadata;
 using System.Runtime.CompilerServices;
+using StudySpark.Core.FileManager;
 
-namespace StudySpark.Core
+namespace StudySpark.Core.Repositories
 {
     // Collection of logic-methods
     // for the SQLite DB
-    public class DBLogic
+    public class FileRepository
     {
-
-        public static SQLiteConnection CreateConnection()
+        private SQLiteConnection conn;
+        public FileRepository()
         {
-
             SQLiteConnection sqlite_conn;
             // Create a new database connection:
             sqlite_conn = new SQLiteConnection("Data Source = ..\\..\\..\\..\\StudySpark.Core\\bin\\Debug\\net6.0\\database.db; Version = 3; New = True; Compress = True; ");
             // Open the connection:
             sqlite_conn.Open();
-            return sqlite_conn;
-        
+            this.conn = sqlite_conn;
+            CreateTable();
         }
 
-        public static void CreateTable(SQLiteConnection conn)
+        private void CreateTable()
         {
 
             SQLiteCommand sqlite_cmd;
@@ -40,10 +40,8 @@ namespace StudySpark.Core
 
         }
 
-        public static void InsertData(SQLiteConnection conn)
+        public void InsertData()
         {
-
-            CreateTable(conn);
             SQLiteCommand sqlite_cmd;
             sqlite_cmd = conn.CreateCommand();
             sqlite_cmd.CommandText = "INSERT INTO FileTable (path, targetname) VALUES('Ditiseentest', 'test'); ";
@@ -51,19 +49,29 @@ namespace StudySpark.Core
 
         }
 
-        public static void ReadData(SQLiteConnection conn)
+        public List<GenericFile> ReadData()
         {
+            List<GenericFile> files = new List<GenericFile>();
+
             SQLiteDataReader reader;
             SQLiteCommand sqlite_cmd;
+            
             sqlite_cmd = conn.CreateCommand();
             sqlite_cmd.CommandText = "SELECT * FROM FileTable";
 
             reader = sqlite_cmd.ExecuteReader();
             while (reader.Read())
             {
-                string myreader = reader.GetString(0);
-                Console.WriteLine(myreader);
+                int dbID = reader.GetInt16("id");
+                string dbPath = reader.GetString("path");
+                string dbTargetName = reader.GetString("targetname");
+                string dbType = reader.GetString("type");
+                string dbImage = reader.GetString("image");
+
+                GenericFile file = new GenericFile(dbID, dbPath, dbTargetName, dbType, dbImage);
+                files.Add(file);
             }
+            return files;
         }
 
     }
