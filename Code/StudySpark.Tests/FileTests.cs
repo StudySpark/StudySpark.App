37488f0b8f956f9b2fab6d29fd7597ebc3aed07e
Tom <tommooitip@gmail.com>
2023-11-23 14:07:41 +0100

    STUD-32 <rewrote test, added test that searches for files in an empty folder>

--- Code/StudySpark.Tests/FileTests.cs
+++ Code/StudySpark.Tests/FileTests.cs
@@ -12,7 +12,16 @@ namespace StudySpark.Tests {
             var extension = ".sln.lnk"; 
             var searchOption = SearchOption.TopDirectoryOnly;
             var expectedFiles = new List<string>();
-            
+
+            if (Directory.Exists(dirPath))
+            {
+                DirectoryInfo di = new DirectoryInfo(dirPath);
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                Directory.Delete(dirPath, true);
+            }
             Directory.CreateDirectory(dirPath);
             
             for(int i = 0; i < 10; i++)
@@ -30,6 +39,39 @@ namespace StudySpark.Tests {
 
             // Check if all expected files are present in the actual result
             foreach(var actualFile in actualFiles)
+            {
+                Assert.Contains(actualFile, expectedFiles);
+            }
+        }
+
+        [Test]
+        public void TestGetFilesFromDirNoFiles()
+        {
+            // Arrange
+            var dirPath = "testdir\\";
+            var extension = ".sln.lnk";
+            var searchOption = SearchOption.TopDirectoryOnly;
+            var expectedFiles = new List<string>();
+
+            if (Directory.Exists(dirPath))
+            {
+                DirectoryInfo di = new DirectoryInfo(dirPath);
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                Directory.Delete(dirPath, true);
+            }
+            Directory.CreateDirectory(dirPath);
+
+            // Act
+            var actualFiles = SearchFiles.GetFilesFromDir(dirPath, extension, searchOption);
+
+            // Assert
+            Assert.That(actualFiles.Count, Is.EqualTo(expectedFiles.Count));
+
+            // Check if all expected files are present in the actual result
+            foreach (var actualFile in actualFiles)
             {
                 Assert.Contains(actualFile, expectedFiles);
             }
