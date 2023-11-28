c04c564388018dd2ef8b3b233294efd05b5848f4
Svenar <github@svenar.nl>
2023-11-28 10:02:42 +0100

    Test BS 2

--- Code/StudySpark.Tests/FileTests.cs
+++ Code/StudySpark.Tests/FileTests.cs
@@ -2,26 +2,31 @@ using StudySpark.Core.FileManager;
 
 namespace StudySpark.Tests {
     [TestFixture]
-    public class FileHelperTests {
+    public class FileHelperTests
+    {
         [Test]
-        public void TestGetFilesFromDir() {
+        public void TestGetFilesFromDir()
+        {
             SearchFiles searchFiles = new();
             // Arrange
-            var dirPath = "testdir\\";
-            var extension = ".sln.lnk";
+            var dirPath = "testdir1\\";
+            var extension = ".sln.lnk"; 
             var searchOption = SearchOption.TopDirectoryOnly;
             var expectedFiles = new List<string>();
 
-            if (Directory.Exists(dirPath)) {
+            if (Directory.Exists(dirPath))
+            {
                 DirectoryInfo di = new DirectoryInfo(dirPath);
-                foreach (FileInfo file in di.GetFiles()) {
+                foreach (FileInfo file in di.GetFiles())
+                {
                     file.Delete();
                 }
                 Directory.Delete(dirPath, true);
             }
             Directory.CreateDirectory(dirPath);
-
-            for (int i = 0; i < 10; i++) {
+            
+            for(int i = 0; i < 10; i++)
+            {
                 string fileToAdd = "test" + i + extension;
                 File.Create(dirPath + fileToAdd);
                 expectedFiles.Add(Path.GetFullPath(dirPath) + fileToAdd);
@@ -34,42 +39,31 @@ namespace StudySpark.Tests {
             Assert.That(actualFiles.Count, Is.EqualTo(expectedFiles.Count));
 
             // Check if all expected files are present in the actual result
-            foreach (var actualFile in actualFiles) {
+            foreach(var actualFile in actualFiles)
+            {
                 Assert.Contains(actualFile, expectedFiles);
             }
         }
 
         [Test]
-        public void TestGetFilesFromDirNoFiles() {
+        public void TestGetFilesFromDirNoFiles()
+        {
             SearchFiles searchFiles = new();
             // Arrange
-            var dirPath = "testdir\\";
+            var dirPath = "testdir2\\";
             var extension = ".sln.lnk";
             var searchOption = SearchOption.TopDirectoryOnly;
             var expectedFiles = new List<string>();
 
-            if (Directory.Exists(dirPath)) {
+            if (Directory.Exists(dirPath))
+            {
                 DirectoryInfo di = new DirectoryInfo(dirPath);
-
-                // Wait for file handles to be released before deleting files
-                foreach (var file in di.GetFiles()) {
-                    file.Refresh();
-                    while (IsFileLocked(file)) {
-                        System.Threading.Thread.Sleep(100);
-                        file.Refresh();
-                    }
+                foreach (FileInfo file in di.GetFiles())
+                {
                     file.Delete();
                 }
-
-                // Wait for file handles to be released before deleting directory
-                while (IsDirectoryLocked(di)) {
-                    System.Threading.Thread.Sleep(100);
-                    di.Refresh();
-                }
-
                 Directory.Delete(dirPath, true);
             }
-
             Directory.CreateDirectory(dirPath);
 
             // Act
@@ -79,31 +73,10 @@ namespace StudySpark.Tests {
             Assert.That(actualFiles.Count, Is.EqualTo(expectedFiles.Count));
 
             // Check if all expected files are present in the actual result
-            foreach (var actualFile in actualFiles) {
+            foreach (var actualFile in actualFiles)
+            {
                 Assert.Contains(actualFile, expectedFiles);
             }
-        }
-
-        private static bool IsFileLocked(FileInfo file) {
-            try {
-                using (FileStream stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None)) {
-                    stream.Close();
-                }
-            } catch (IOException) {
-                return true;
-            }
-
-            return false;
-        }
-
-        private static bool IsDirectoryLocked(DirectoryInfo directory) {
-            try {
-                var _ = directory.GetFiles();
-            } catch (IOException) {
-                return true;
-            }
-
-            return false;
         }
     }
 }
