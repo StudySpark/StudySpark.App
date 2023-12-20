82e115c0f0a9a10f559f95922f95b1568a2c55bc
CarlijnGJ <carlijn.blom@windesheim.nl>
2023-12-20 23:08:46 +0100

    Added constructor and relaycommand to ScheduleViewModel

--- Code/StudySpark.GUI.WPF/MVVM/ViewModel/ScheduleViewModel.cs
+++ Code/StudySpark.GUI.WPF/MVVM/ViewModel/ScheduleViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StudySpark.GUI.WPF.Core;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -9,5 +10,18 @@ namespace StudySpark.GUI.WPF.MVVM.ViewModel {
     public class ScheduleViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        public RelayCommand ScheduleLoginCommand { get; set; }
+
+        public ScheduleViewModel() 
+        {
+
+            ScheduleLoginCommand = new RelayCommand(o => {
+                MainViewManager.CurrentMainView = MainViewManager.LoginVM;
+                //CurrentView = LoginVM;
+            });
+
+        }
+
     }
 }
