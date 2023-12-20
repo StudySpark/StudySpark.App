2bdc51fd601300c879a21cb1d36baa1cb1aa236b
CarlijnGJ <carlijn.blom@windesheim.nl>
2023-12-20 15:26:00 +0100

    Setup ScheduleView and ScheduleViewModel
    - Moved 'Rooster'-button up so it's below 'Cijfers' in MainWindow
    - Started a grid in ScheduleView, creating it doesn't work fully yet
    - Added CreateGrid()-method in ScheduleView for the time being
    - Added INotifyPropertyChanged to ScheduleViewModel

--- Code/StudySpark.GUI.WPF/MVVM/View/ScheduleView.xaml.cs
+++ Code/StudySpark.GUI.WPF/MVVM/View/ScheduleView.xaml.cs
@@ -20,9 +20,41 @@ namespace StudySpark.GUI.WPF.MVVM.View
     /// </summary>
     public partial class ScheduleView : UserControl
     {
+
+        private Grid ScheduleGrid;
+
         public ScheduleView()
         {
             InitializeComponent();
+            CreateGrid();
+        }
+
+        public void CreateGrid()
+        {
+            ScheduleGrid = new Grid();
+
+            for (int row = 0; row < 6; row++)
+            {
+                ScheduleGrid.RowDefinitions.Add(new RowDefinition());
+                for (int col = 0; col < 6; col++)
+                {
+                    ScheduleGrid.ColumnDefinitions.Add(new ColumnDefinition());
+
+                    // Create a Border control for each square
+                    Border border = new Border
+                    {
+                        BorderBrush = Brushes.Gray,
+                        BorderThickness = new Thickness(1),
+                    };
+
+                    // Add the Border control to the mainGrid
+                    MainSchedule.Children.Add(border);
+
+                    // Set the row and column for the Border control
+                    Grid.SetRow(border, row);
+                    Grid.SetColumn(border, col);
+                }
+            }
         }
     }
 }
--- Code/StudySpark.GUI.WPF/MVVM/ViewModel/ScheduleViewModel.cs
+++ Code/StudySpark.GUI.WPF/MVVM/ViewModel/ScheduleViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StudySpark.GUI.WPF.MVVM.ViewModel {
-    public class ScheduleViewModel {
+    public class ScheduleViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
