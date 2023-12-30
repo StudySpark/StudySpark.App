45f56d4d2a697f6234c5a74cb4a6ad5c5b3ff15b
CarlijnGJ <carlijn.blom@windesheim.nl>
2023-12-30 17:42:20 +0100

    Drafted base for WIPWebScraper

---
+++ Code/StudySpark.WebScraper/WIP/WIPWebScraper.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudySpark.WebScraper.WIP
+{
+    internal class WIPWebScraper : WebScraper
+    {
+
+        public WIPWebScraper(ScraperOptions scraperOptions) : base(scraperOptions)
+        {
+            scraperOptions.URL = "https://wip.windesheim.nl";
+        }
+
+        public void Load()
+        {
+            SetupDriver();
+            WaitForPageLoad();
+
+            bool accRegistered = CheckForAccounts();
+        }
+
+        private bool CheckForAccounts()
+        {
+
+            List<IWebElement> accButtons = driver.FindElements(By.ClassName("table")).ToList();
+
+            foreach (IWebElement accButton in accButtons) 
+            { 
+                Debug.WriteLine(accButton);
+            }
+
+            if (accButtons.Count > 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
