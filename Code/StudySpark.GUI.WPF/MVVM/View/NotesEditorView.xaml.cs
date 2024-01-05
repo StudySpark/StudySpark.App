9a3dd6ba22fb87c6cfbec9f7db1970a7429a9bfe
Svenar <github@svenar.nl>
2024-01-05 21:57:22 +0100

    Caching editor content and waiting until the GUi is actually loaded before pushing the editor content

--- Code/StudySpark.GUI.WPF/MVVM/View/NotesEditorView.xaml.cs
+++ Code/StudySpark.GUI.WPF/MVVM/View/NotesEditorView.xaml.cs
@@ -10,36 +10,50 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xaml;
 
 namespace StudySpark.GUI.WPF.MVVM.View {
     /// <summary>
     /// Interaction logic for NotesEditorView.xaml
     /// </summary>
     public partial class NotesEditorView : UserControl {
-       
+
+        private static string content = "";
+
         public NotesEditorView() {
             InitializeComponent();
 
             NotesEditorViewModel.LoadContentEvent += HandleLoadContentEvent;
+
+            Loaded += windowLoaded;
         }
 
-        private void HandleLoadContentEvent(object sender, LoadContentEventArgs e)
-        {
-            // Handle the loaded content here
-            string loadedContent = e.Content;
+        private void windowLoaded(object sender, RoutedEventArgs e) {
+            loadRTF();
+        }
 
-            // Example: Display the content in the RichTextBox
-            using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(loadedContent)))
-            {
-                TextRange text = new TextRange(rtfEditor.Document.ContentStart, rtfEditor.Document.ContentEnd);
-                text.Load(stream, DataFormats.Rtf);
+        private void HandleLoadContentEvent(object sender, LoadContentEventArgs e) {
+            content = e.Content;
+        }
+
+        void loadRTF() {
+            if (content.Length == 0) {
+                return;
             }
+
+            using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(content))) {
+                stream.Seek(0, SeekOrigin.Begin);
+                rtfEditor.SelectAll();
+                rtfEditor.Selection.Load(stream, DataFormats.Rtf);
+            }
+
+            content = "";
         }
+
     }
 }
